a0d058ff1d2a3348972757bd2ffe89af1420c996
Andrea Magni <andrea.magni@gmail.com>
2019-10-29 15:37:49 +0100

    Typo

--- UI.Consts.pas
+++ UI.Consts.pas
@@ -10,11 +10,11 @@ type
   public
     const MATERIAL_BLUE_800   = $FF1565C0;
     const MATERIAL_RED_800    = $FFc62828;
-    const MATERIA_AMBER_800   = $FFff8f00;
+    const MATERIAL_AMBER_800   = $FFff8f00;
     const MATERIAL_INDIGO_800 = $FF283593;
     const MATERIAL_PURPLE_800 = $FF6a1b9a;
 
-    const PRIMARY_COLOR = MATERIA_AMBER_800;
+    const PRIMARY_COLOR = MATERIAL_AMBER_800;
     const PRIMARY_TEXT_COLOR = TAlphaColorRec.White;
     const SOLID_BACKGROUND_COLOR = TAlphaColorRec.Lightgray;
     const LIGHT_BACKGROUND_COLOR = TAlphaColorRec.Silver;
