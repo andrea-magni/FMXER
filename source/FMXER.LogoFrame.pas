b0bee0ea3a92ef4c6ab853be71df7a51b6e1dd0a
Andrea Magni <andrea.magni@gmail.com>
2021-12-14 12:07:34 +0100

    Added LogoResource to TLogoFrame

--- source/FMXER.LogoFrame.pas
+++ source/FMXER.LogoFrame.pas
@@ -3,7 +3,7 @@ unit FMXER.LogoFrame;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
   FMX.Objects;
 
@@ -11,13 +11,30 @@ type
   TLogoFrame = class(TFrame)
     LogoImage: TImage;
   private
-    { Private declarations }
+    FLogoResource: string;
+    procedure SetLogoResource(const Value: string);
   public
-    { Public declarations }
+    property LogoResource: string read FLogoResource write SetLogoResource;
   end;
 
 implementation
 
 {$R *.fmx}
+
+{ TLogoFrame }
+
+procedure TLogoFrame.SetLogoResource(const Value: string);
+begin
+  if FLogoResource <> Value then
+  begin
+    FLogoResource := Value;
+    var LStream := TResourceStream.Create(hInstance, FLogoResource, RT_RCDATA);
+    try
+      LogoImage.Bitmap.LoadFromStream(LStream);
+    finally
+      LStream.Free;
+    end;
+  end;
+end;
 
 end.
