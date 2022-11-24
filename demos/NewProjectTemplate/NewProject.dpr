a5609a9900d0302156224e4fbd3b1b3d52c50e0f
Andrea Magni <andrea.magni@gmail.com>
2022-11-24 17:18:33 +0100

    Demo sync

--- demos/NewProjectTemplate/NewProject.dpr
+++ demos/NewProjectTemplate/NewProject.dpr
@@ -3,14 +3,12 @@ program NewProject;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  Skia.FMX,
   Forms.Main in 'Forms.Main.pas' {MainForm},
   Routes.home in 'Routes.home.pas';
 
 {$R *.res}
 
 begin
-  GlobalUseSkia := True;
   Application.Initialize;
   Application.CreateForm(TMainForm, MainForm);
   Application.Run;
