092f59ce71d033e06dd464cdf7fabf6a087b4619
Andrea Magni <andrea.magni@gmail.com>
2022-11-24 11:51:07 +0100

    Enabled Skia in NewProjectTemplate

--- demos/NewProjectTemplate/NewProject.dpr
+++ demos/NewProjectTemplate/NewProject.dpr
@@ -3,12 +3,14 @@ program NewProject;
 uses
   System.StartUpCopy,
   FMX.Forms,
+  Skia.FMX,
   Forms.Main in 'Forms.Main.pas' {MainForm},
   Routes.home in 'Routes.home.pas';
 
 {$R *.res}
 
 begin
+  GlobalUseSkia := True;
   Application.Initialize;
   Application.CreateForm(TMainForm, MainForm);
   Application.Run;
